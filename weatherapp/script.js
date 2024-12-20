57be0db894915d1938bb115520d75da0f0a4f00a
Kaustubh-S8 <satamkaustubh2003@gmail.com>
2024-12-20 09:17:30 -0800

    script.js

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -11,32 +11,51 @@ async function fetchWeatherData(cityName) {
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=18b20493f0c0fac19c54e3e0d8bc6ba7`
     );
     let finalData = await data.json();
+    
     let displayData = document.querySelector(".display-data");
     let displayTemp = document.querySelector(".display-temp");
     let displayLocation = document.querySelector(".display-loc");
     let displaySpeed = document.querySelector(".display-speed");
     let displayHumidity = document.querySelector(".display-humidity");
     let icon = document.querySelector("#icon");
+    let asideDataDisappear = document.querySelectorAll(".inactive");
+    
     if (finalData.cod == "404") {
-      let asideDataDisappear = document.querySelectorAll(".inactive");
+      
       [...asideDataDisappear].forEach((v, i) => {
         v.style.display = "none";
-        v.style.visibility = "none";
       });
+      
       displayData.innerHTML =
         "Location is not valid. Please enter valid Location";
       displayData.style.color = "red";
-    } else {
+    
+    }
+    else {
       console.log(finalData);
       console.log(displayTemp);
+    
       let { name, main, wind, weather } = finalData;
+      
       displayTemp.innerHTML = parseInt(main.temp - 273.15) + "°C";
       displayLocation.innerHTML = name;
       displaySpeed.innerHTML = wind.speed + "km/hr";
       displayHumidity.innerHTML = main.humidity + "%";
       icon.src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+       [...asideDataDisappear].forEach((v, i) => {
+        v.style.display = "flex";
+      });
+
+      displayData.innerHTML =
+        "Weather App";
+      displayData.style.color = "black";
+    
     }
-  } catch (error) {
+  } 
+  catch (error) {
+  
     console.log("err");
+  
   }
 }
